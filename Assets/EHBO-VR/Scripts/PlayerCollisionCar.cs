fa12fa744b724571c513fbf1f3eb4d19513b3a96
Cas Bouwmans <161725595+CasBouwmans@users.noreply.github.com>
2025-11-04 09:27:34 +0100

    Changed playercollision script name

--- Assets/EHBO-VR/Scripts/PlayerCollisionCar.cs
+++ Assets/EHBO-VR/Scripts/PlayerCollisionCar.cs
@@ -1,19 +1,24 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
+public class PlayerCollisionCar : MonoBehaviour
 {
     private bool gotHit = false;
     public TextMeshProUGUI hitText;
-   
+
+    private void Start()
+    {
+        hitText.enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Car") && !gotHit)
-        {    
+        {
             Debug.Log("Je bent aangereden door een auto: " + other.gameObject.name);
             gotHit = true;
             hitText.enabled = true;
-        
+
         }
     }
 }
