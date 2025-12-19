f95a103413d33fd84fb2c4d84a17fc3cdd71b4a8
Cas Bouwmans <161725595+CasBouwmans@users.noreply.github.com>
2025-12-19 12:58:39 +0100

    Made a achievement design

--- Assets/EHBO-VR/Scripts/Achievement.cs
+++ Assets/EHBO-VR/Scripts/Achievement.cs
@@ -1,27 +1,15 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class Achievement : MonoBehaviour
 {
     [Header("Feedback Settings")]
-    [SerializeField] private AudioClip achievementSound;       // Geluid bij voltooien taak
-    [SerializeField] private Canvas achievementCanvas;         // Canvas die kort getoond wordt
-    [SerializeField] private float canvasDisplayDuration = 2f;// Tijd dat canvas zichtbaar blijft
     [SerializeField] private TMP_Text achievementText;
-
-    void Start()
-    {
-        if (achievementCanvas != null)
-        {
-            achievementCanvas.gameObject.SetActive(false);
-        }
-            
-    }
+    [SerializeField] private Animator animator;
 
     public void UnlockAchievement(int completedStep)
     {
-        if (achievementText == null || achievementCanvas == null)
+        if (achievementText == null || animator == null)
             return;
 
         switch (completedStep)
@@ -29,27 +17,17 @@ public class Achievement : MonoBehaviour
             case 1:
                 achievementText.text = "Achievement Unlocked!\nTimer onder controle";
                 break;
-
             case 2:
                 achievementText.text = "Achievement Unlocked!\nVR Handen Meester";
                 break;
-
             case 3:
                 achievementText.text = "Achievement Unlocked!\nHandgebaren Expert";
                 break;
-
             default:
-                return; // Geen achievement voor andere stappen
+                return;
         }
 
-        StopAllCoroutines();
-        StartCoroutine(ShowCanvasTemporarily());
-    }
-
-    private IEnumerator ShowCanvasTemporarily()
-    {
-        achievementCanvas.gameObject.SetActive(true);
-        yield return new WaitForSeconds(canvasDisplayDuration);
-        achievementCanvas.gameObject.SetActive(false);
+        // Speel de animatie vanaf het begin
+        animator.Play("Achievement", 0, 0f);
     }
 }
