49ef349bdf61595b2107147982444a85234c8e28
Cas Bouwmans <161725595+CasBouwmans@users.noreply.github.com>
2026-01-07 09:36:16 +0100

    Changed InfoText in game tutorial

--- Assets/EHBO-VR/Scripts/GameTutorialManager.cs
+++ Assets/EHBO-VR/Scripts/GameTutorialManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class GameTutorialManager : MonoBehaviour
 {
@@ -94,14 +95,14 @@ public class GameTutorialManager : MonoBehaviour
             case 0:
                 tutorialTexts[0].text = "0/3";
                 tutorialTexts[1].text = "Welkom bij de EHBO VR tutorial!";
-                tutorialTexts[2].text = "In deze tutorial zullen we je basis functies uitleggen van de game.";
+                tutorialTexts[2].text = "3 basis functies worden uitgelegd.";
                 timeManager.ResetTimer(0f);
                 timerCanvas.gameObject.SetActive(false);
                 break;
             case 1:
                 tutorialTexts[0].text = "1/3";
                 tutorialTexts[1].text = "Timer introductie";
-                tutorialTexts[2].text = "De timer geeft aan hoe lang je nog hebt om het slachtoffer te redden in de game.";
+                tutorialTexts[2].text = "De timer geeft aan hoe lang je nog hebt om het slachtoffer te redden.";
                 timeManager.ResetTimer(15f);
                 timerCanvas.gameObject.SetActive(true);
                 videoPlayerVRHands.SetActive(false);
@@ -110,7 +111,9 @@ public class GameTutorialManager : MonoBehaviour
             case 2:
                 tutorialTexts[0].text = "2/3";
                 tutorialTexts[1].text = "VR handen introductie";
-                tutorialTexts[2].text = "Tijdens het de game zul je een aantal handelingen moeten uitvoeren. Deze handelingen zul je kunnen doen met je handen in VR.";
+                tutorialTexts[2].text = "Tijdens de game moet je een aantal handelingen uitvoeren.\n\n" + "Deze voer je uit met je VR-handen.";
+
+
                 timeManager.ResetTimer(0f);
                 timerCanvas.gameObject.SetActive(false);
                 videoPlayerHandGestures.SetActive(false);
@@ -121,7 +124,7 @@ public class GameTutorialManager : MonoBehaviour
             case 3:
                 tutorialTexts[0].text = "3/3";
                 tutorialTexts[1].text = "VR hand gebaren introductie";
-                tutorialTexts[2].text = "Je zult een aantal hand gebaren moeten gebruiken tijdens het scenario.";
+                tutorialTexts[2].text = "Je zult een aantal hand gebaren moeten gebruiken.";
                 videoPlayerHandGestures.SetActive(true);
                 videoPlayerVRHands.SetActive(false);
                 toGameButton.SetActive(false);
@@ -130,7 +133,7 @@ public class GameTutorialManager : MonoBehaviour
             case 4:
                 tutorialTexts[0].text = "";
                 tutorialTexts[1].text = "Tutorial voltooid!";
-                tutorialTexts[2].text = "je kunt nu door naar de game door te klikken op de rechter knop";
+                tutorialTexts[2].text = "Je kunt nu verder met de game.\n\n" + "Klik op de rechterknop 'Naar de game'.";
                 videoPlayerHandGestures.SetActive(false);
                 toGameButton.SetActive(true);
                 break;
