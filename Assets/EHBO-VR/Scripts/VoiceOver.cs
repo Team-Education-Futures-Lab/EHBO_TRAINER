21627ef9b503781f2864f917ca03200a2e8865a7
Cas Bouwmans <161725595+CasBouwmans@users.noreply.github.com>
2026-01-08 16:59:56 +0100

    Added Game menu voice over

--- Assets/EHBO-VR/Scripts/VoiceOver.cs
+++ Assets/EHBO-VR/Scripts/VoiceOver.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class VoiceOver : MonoBehaviour
 {
     [SerializeField] private AudioSource voiceOverSource;
+    [SerializeField] private AudioClip IntroTutorialsClip;       // de voice over audio van de intro/menu
+    [SerializeField] private float delayIntroVoiceOver = 2f;
+    void Start()
+    {
+        if (IntroTutorialsClip != null)
+        {
+            StartCoroutine(PlayVoiceOverWithDelay(IntroTutorialsClip, delayIntroVoiceOver));
+        }
+    }
+
+    private IEnumerator PlayVoiceOverWithDelay(AudioClip clip, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        PlayVoiceOver(clip); // gewoon dit script zelf gebruiken
+    }
 
     public void PlayVoiceOver(AudioClip clip)
     {
